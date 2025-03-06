46d8613c39daaaabaefba0ece174ff8a82569da4
MarceloNaviaR <marcelo.navia@gmail.com>
2025-03-06 09:48:41 -0400

    Correccion mostrar

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,9 +1,9 @@
 function precioNeto(a, b) {
-return parseFloat(precio.toFixed(2)); 
+  let precio = a * b;
+  return parseFloat(precio.toFixed(2)); // Convierte el string de vuelta a número
 }
-
 function mostrar(cantidad,precio){
-  let precio_n = precio_neto(cantidad,precio);
+  let precio_n = precioNeto(cantidad,precio);
   let mostrar_p = "La cantidad es: " + cantidad  + "<br>" + "El precio por unidad es: " + precio + "<br>" + "El precio neto sera de (" + cantidad + "*"+ precio + "$): " + precio_n+"$";
   return mostrar_p; 
 }
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -4,6 +4,5 @@ describe("Mostrara el precio neto del producto", () => {
   it("Muestra la cantidad del producto ingresado", () => {
     expect(precioNeto(7,5)).toEqual(35);
   });
-})
-
+});
 
