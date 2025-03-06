9f6ce3a4fe36344171f2c97c684a85c60c7cf808
MarceloNaviaR <marcelo.navia@gmail.com>
2025-03-06 11:46:47 -0400

    Funcion impuesto de estado

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,10 +1,26 @@
 function precioNeto(a, b) {
   let precio = a * b;
-  return parseFloat(precio.toFixed(2)); // Convierte el string de vuelta a número
+  return parseFloat(precio.toFixed(2)); 
 }
-function mostrar(cantidad,precio){
+
+function obtener_impuesto_estado(cod_estado){
+  let impuesto_estado;
+  switch (cod_estado) {
+    case "CA":
+      impuesto_estado = 0.0825;
+      break;
+  }
+  return impuesto_estado;
+}
+
+function mostrar(cantidad,precio,cod_estado){
   let precio_n = precioNeto(cantidad,precio);
-  let mostrar_p = "La cantidad es: " + cantidad  + "<br>" + "El precio por unidad es: " + precio + "<br>" + "El precio neto sera de (" + cantidad + "*"+ precio + "$): " + precio_n+"$";
+  let impuesto_estado = obtener_impuesto_estado(cod_estado);
+  let mostrar_p = "La cantidad es: " + cantidad  + "<br>" + 
+  "El precio por unidad es: " + precio + "<br>" +
+  "Codigo de estado es: " + cod_estado + "<br>"+
+  "El precio neto sera de (" + cantidad + "*"+ precio + "$): " + precio_n+"$" + "<br>" +
+  "Impuesto para "+cod_estado+"(%"+impuesto_estado+"): ";
   return mostrar_p; 
 }
 
