01f16f6ccac301066c51065af2c649b77d1f489e
Diego Rios <diegoarv08@hotmail.com>
2025-03-09 22:09:01 -0400

    Validacion de datos nulos y correccion de descripcion de pruebas

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -6,20 +6,20 @@ describe("Calculara el precio neto del producto", () => {
   });
 });
 
-describe("Mostrara el impuesto segun el estado", () => {
-  it("Muestra el impuesto segun el estado de CA", () => {
+describe("Calcula el impuesto segun el estado", () => {
+  it("Calcula el impuesto segun el estado de CA", () => {
     expect(calcular_impuesto_estado(10, "CA")).toEqual(0.83);
   });
-  it("Muestra el impuesto segun el estado de AL", () => {
+  it("Calcula el impuesto segun el estado de AL", () => {
     expect(calcular_impuesto_estado(10, "AL")).toEqual(0.4);
   });
-  it("Muestra el impuesto segun el estado de NV", () => {
+  it("Calcula el impuesto segun el estado de NV", () => {
     expect(calcular_impuesto_estado(10, "NV")).toEqual(0.8);
   });
-  it("Muestra el impuesto segun el estado de UT", () => {
+  it("Calcula el impuesto segun el estado de UT", () => {
     expect(calcular_impuesto_estado(10, "UT")).toEqual(0.67);
   });
-  it("Muestra el impuesto segun el estado de TX", () => {
+  it("Calcula el impuesto segun el estado de TX", () => {
     expect(calcular_impuesto_estado(10, "TX")).toEqual(0.63);
   });
 });
@@ -42,64 +42,64 @@ describe("Mostrar el descuento segun el precio neto", () => {
   });
 });
 
-describe("Calcula el impuesto en en las categorías", () => {
+describe("Muestra correctamente el impuesto en en las categorías", () => {
   it("Categoria Alimentos", () => {
-    const resultado1 = mostrar(10, 5, "CA", "Alimentos");
+    const resultado1 = mostrar(10, 5, "CA", "Alimentos",1,"Normal");
     expect(resultado1).toContain("Impuesto por categoría (0.00%): +$0");
   });
   it("Categoria Bebidas Alcoholicas", () => {
-    const resultado1 = mostrar(20, 5, "AL", "Bebidas Alcoholicas");
+    const resultado1 = mostrar(20, 5, "AL", "Bebidas Alcoholicas",1,"Normal");
     expect(resultado1).toContain("Impuesto por categoría (7.00%): +$7");
   });
   it("Categoria Material Escritorio", () => {
-    const resultado1 = mostrar(30, 7, "NV", "Material Escritorio");
+    const resultado1 = mostrar(30, 7, "NV", "Material Escritorio",1,"Normal");
     expect(resultado1).toContain("Impuesto por categoría (0.00%): +$0");
   });
   it("Categoria Muebles", () => {
-    const resultado1 = mostrar(35, 600, "UT", "Muebles");
+    const resultado1 = mostrar(35, 600, "UT", "Muebles",1,"Normal");
     expect(resultado1).toContain("Impuesto por categoría (3.00%): +$630");
   });
   it("Categoria Electronicos", () => {
-    const resultado1 = mostrar(5, 1090, "TX", "Electronicos");
+    const resultado1 = mostrar(5, 1090, "TX", "Electronicos",1,"Normal");
     expect(resultado1).toContain("Impuesto por categoría (4.00%): +$218");
   });
   it("Categoria Vestimenta", () => {
-    const resultado1 = mostrar(120, 90, "TX", "Vestimenta");
+    const resultado1 = mostrar(120, 90, "TX", "Vestimenta",1,"Normal");
     expect(resultado1).toContain("Impuesto por categoría (2.00%): +$216");
   });
   it("Categoria Varios", () => {
-    const resultado1 = mostrar(140, 64.50, "UT", "Varios");
+    const resultado1 = mostrar(140, 64.50, "UT", "Varios",1,"Normal");
     expect(resultado1).toContain("Impuesto por categoría (0.00%): +$0");
   });
 });
 
-describe("Calcula el Descuento en en las categorías", () => {
+describe("Muestra correctamente el Descuento en en las categorías", () => {
   it("Categoria Alimentos", () => {
-    const resultado1 = mostrar(10, 5, "CA", "Alimentos");
+    const resultado1 = mostrar(10, 5, "CA", "Alimentos",1,"Normal");
     expect(resultado1).toContain("Descuento Categoria (2.00%): -$1");
   });
   it("Categoria Bebidas Alcoholicas", () => {
-    const resultado1 = mostrar(20, 5, "AL", "Bebidas Alcoholicas");
+    const resultado1 = mostrar(20, 5, "AL", "Bebidas Alcoholicas",1,"Normal");
     expect(resultado1).toContain("Descuento Categoria (0.00%): -$0");
   });
   it("Categoria Material Escritorio", () => {
-    const resultado1 = mostrar(30, 7, "NV", "Material Escritorio");
+    const resultado1 = mostrar(30, 7, "NV", "Material Escritorio",1,"Normal");
     expect(resultado1).toContain("Descuento Categoria (1.50%): -$3.15");
   });
   it("Categoria Muebles", () => {
-    const resultado1 = mostrar(35, 600, "UT", "Muebles");
+    const resultado1 = mostrar(35, 600, "UT", "Muebles",1,"Normal");
     expect(resultado1).toContain("Descuento Categoria (0.00%): -$0");
   });
   it("Categoria Electronicos", () => {
-    const resultado1 = mostrar(5, 1090, "TX", "Electronicos");
+    const resultado1 = mostrar(5, 1090, "TX", "Electronicos",1,"Normal");
     expect(resultado1).toContain("Descuento Categoria (1.00%): -$54.5");
   });
   it("Categoria Vestimenta", () => {
-    const resultado1 = mostrar(120, 90, "TX", "Vestimenta");
+    const resultado1 = mostrar(120, 90, "TX", "Vestimenta",1,"Normal");
     expect(resultado1).toContain("Descuento Categoria (0.00%): -$0");
   });
   it("Categoria Varios", () => {
-    const resultado1 = mostrar(140, 64.50, "UT", "Varios");
+    const resultado1 = mostrar(140, 64.50, "UT", "Varios",1,"Normal");
     expect(resultado1).toContain("Descuento Categoria (0.00%): -$0");
   });
 });
@@ -112,7 +112,7 @@ describe("Calculara el peso volumetrico del envio", () => {
 
 describe("Mostrara el costo del envio", () => {
   it("Peso entre 0 a 10 kilos", () => {
-    const resultado1 = mostrar(10, 5, "CA", "Alimentos", 0.15);
+    const resultado1 = mostrar(10, 5, "CA", "Alimentos", 0.15,"Normal");
     expect(resultado1).toContain("Precio del costo de envio (1.5kg.): +$0");
   });
   it("Peso entre 11 a 20 kilos", () => {
@@ -120,23 +120,23 @@ describe("Mostrara el costo del envio", () => {
     expect(resultado1).toContain("Precio del costo de envio (20kg.): +$3.5");
   });
   it("Peso entre 21 a 40 kilos", () => {
-    const resultado1 = mostrar(4, 8, "TX", "Varios", 8.1);
+    const resultado1 = mostrar(4, 8, "TX", "Varios", 8.1,"Normal");
     expect(resultado1).toContain("Precio del costo de envio (32.4kg.): +$5");
   });
   it("Peso entre 41 a 80 kilos", () => {
-    const resultado1 = mostrar(1, 260, "TX", "Varios", 61.4);
+    const resultado1 = mostrar(1, 260, "TX", "Varios", 61.4,"Normal");
     expect(resultado1).toContain("Precio del costo de envio (61.4kg.): +$6");
   });
   it("Peso entre 80 a 100 kilos", () => {
-    const resultado1 = mostrar(6, 11.60, "TX", "Varios", 20.5);
+    const resultado1 = mostrar(6, 11.60, "TX", "Varios", 20.5,"Normal");
     expect(resultado1).toContain("Precio del costo de envio (123kg.): +$8");
   });
   it("Peso entre 101 a 200 kilos", () => {
-    const resultado1 = mostrar(6, 11.60, "TX", "Varios", 20.5);
+    const resultado1 = mostrar(6, 11.60, "TX", "Varios", 20.5,"Normal");
     expect(resultado1).toContain("Precio del costo de envio (123kg.): +$8");
   });
   it("Peso mas de 200 kilos", () => {
-    const resultado1 = mostrar(4, 270, "TX", "Electronico", 62.8);
+    const resultado1 = mostrar(4, 270, "TX", "Electronico", 62.8,"Normal");
     expect(resultado1).toContain("Precio del costo de envio (251.2kg.): +$9");
   });
 });
@@ -207,5 +207,8 @@ describe("Validadcion de datos al enviar a la funcion mostrar", () => {
   });
   it("Muestra mensaje de Error al enviar un 0", () => {
     expect(mostrar(12, 0,"CA", "Alimentos", 0.2, "Recurrente" )).toEqual("Error: Se ingresaron datos erroneos");
+  });
+  it("Muestra mensaje de Error al enviar un dato null", () => {
+    expect(mostrar(null, null, "CA", "Alimentos", null, "Recurrente" )).toEqual("Error: Se ingresaron datos erroneos");
   });
 });
